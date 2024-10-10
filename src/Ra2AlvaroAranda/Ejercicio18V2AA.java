4353d54b2b394a28783a0b75e22e487c083dcc00
Alvarlo <alvaro.s401816@cesurformacion.com>
2024-10-10 11:00:16 +0200

    Ejercicio 18V2

---
+++ src/Ra2AlvaroAranda/Ejercicio18V2AA.java
@@ -0,0 +1,16 @@
+package Ra2AlvaroAranda;
+
+public class Ejercicio18V2AA {
+    public static void main(String[] args) {
+        /*
+        Imprimir en consola todos los numeros multiplos de 2. El cliclo finalizará cuando el último
+        múltiplo sea menor a 4570
+         */
+
+        int i = 0;
+        while (i*2 < 4570){
+            System.out.println(i*2);
+            i++;
+        }
+    }
+}
