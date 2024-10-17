bedb45e2b915f2f2657dfc6e380a9a3e4686fbea
Alvarlo <alvaro.s401816@cesurformacion.com>
2024-10-17 11:46:44 +0200

    17-10-1146

--- src/AceptaElReto/AceptaElReto4.java
+++ src/AceptaElReto/AceptaElReto4.java
@@ -1,2 +1,28 @@
-package AceptaElReto;public class AceptaElReto4 {
+package AceptaElReto;
+
+public class AceptaElReto4 {
+    public static void main(String[] args) {
+        /*
+        Cuando los ninos aprenden a contar, siempre empiezan su cantinela con el numero "uno". Hacerlo empezando en 0
+         no tiene sentido porque el mero hecho de contar asume que hay algo para ser contado y, por tanto, al menos
+         habra 1. Esto es asi hasta tal punto que los numeros naturales de los matematicos no incluian el 0 y, de hecho
+         , dependiendo del ambito de uso, en algunos contextos aun se asume que no lo incluyen.
+
+        Y es que aunque hoy pueda resultar sorprendente, el concepto del cero ha sido historicamente complejo y
+        tardo mucho
+         en asentarse. Aunque aparecio timidamente con anterioridad, a Europa el cero posicional llego gracias a
+         Fibonacci en el siglo XII con su libro Liber abaci ("El libro del abaco") donde introducia el algebra arabe
+          (copiada, a su vez, de los indios).
+
+        Anteriormente, varias civilizaciones antiguas disponian de simbolos para representarlo (el Antiguo Egipto, Babilonia,
+         la Antigua Grecia y la civilizacion Maya, por ejemplo) pero no aprovecharon su verdadero potencial. Por su parte, los
+         romanos, que fueron grandes legisladores y arquitectos, fueron malos matematicos, y en los numeros romanos no habia
+         forma de representar el 0.
+
+        Ante esta situacion, no es de extranar que no existiera el ano 0. El ano 1 antes de Cristo fue seguido por el ano 1
+        despues de Cristo. En realidad, quien impulso este modo de contar los anos fue Beda, un monje benedictino nacido en
+         el siglo VII que si intuia la existencia del cero. Pero, penso, si no hay "mes cero", ni "dia de la semana cero",
+         ni "siglo cero", ¿por que ha de haber "ano cero"?
+         */
+    }
 }
--- src/Apuntes/Apuntes6for.java
+++ src/Apuntes/Apuntes6for.java
@@ -1,2 +1,11 @@
-package Apuntes;public class Apuntes6for {
+package Apuntes;
+
+public class Apuntes6for {
+    public static void main(String[] args) {
+
+        int num = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + i*num);
+        }
+    }
 }
